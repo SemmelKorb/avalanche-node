0bdcb55f5e53c91933ceff552c03099c2b982046
Semmelon <138894594+Semmelon@users.noreply.github.com>
2025-01-07 12:59:15 +0100

    small bugfixes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { Pool } = require('pg');
 const app = express();
 const port = 3000;
-const elias = 1;
+
 app.use(cors({
     origin: 'http://localhost:7319'
 }));
@@ -11,9 +11,9 @@ app.use(express.json());
 
 const pool = new Pool({
     host: 'localhost',
-    user: 'postgres',
-    password: 'postgres',
-    database: 'notes_db',
+    user: 'root',
+    password: 'root',
+    database: 'yttrium',
     port: 5432
 });
 
@@ -21,12 +21,14 @@ app.get('/', (req, res) => {
     res.send('Welcome to PaulHackerPage:');
 });
 
-app.get('/notes:offset', async (req, res) => {
-    const offset = req.params.offset
+app.get('/notes/:offset', async (req, res) => {
+    const page = req.params.offset * 25
+    console.log()
     try {
-        const { rows } = await pool.query('SELECT * FROM notes OFFSET = $1 LIMIT = 25', [offset]);
+        const { rows } = await pool.query('SELECT * FROM notes OFFSET $1 LIMIT 25', [page]);
         res.send(rows);
     } catch (err) {
+        console.log(err)
         res.status(500).send('Error fetching notes from database');
     }
 });
@@ -40,7 +42,7 @@ app.get('/notesPinned', async (req, res) => {
     }
 });
 
-app.get('/notes/:id', async (req, res) => {
+app.get('/note/:id', async (req, res) => {
     const noteId = req.params.id;
     try {
         const { rows } = await pool.query('SELECT * FROM notes WHERE id = $1', [noteId]);
@@ -54,7 +56,7 @@ app.get('/notes/:id', async (req, res) => {
     }
 });
 
-app.put('/notes/:id', async (req, res) => {
+app.put('/note/:id', async (req, res) => {
     const noteId = req.params.id;
     const { title, content } = req.body;
 
@@ -73,7 +75,7 @@ app.put('/notes/:id', async (req, res) => {
     }
 });
 
-app.post('/notes', async (req, res) => {
+app.post('/note', async (req, res) => {
     const { title, content, isFav, creationDate } = req.body;
 
     try {
@@ -82,7 +84,8 @@ app.post('/notes', async (req, res) => {
             [title, content, isFav, creationDate]
         );
         const id = rows[0].id;
-        res.status(200).json({ id: id, message: `Note created with ID: ${id}` });
+        const creationdate = rows[0].creationDate
+        res.status(200).json({ id: id, creationDate: creationdate, message: `Note created with ID: ${id}` });
     } catch (err) {
         console.error('Error inserting note:', err);
         res.status(500).json({ error: 'Error inserting note' });
@@ -93,7 +96,7 @@ app.delete('/delete/:id', async (req, res) => {
     const noteId = req.params.id;
 
     try {
-        const { rowCount } = await pool.query('DELETE FROM notes WHERE id = $1', [noteId]);
+        const { rowCount } = await pool.query('DELETE FROM notes WHERE uuid = $1', [noteId]);
         if (rowCount === 0) {
             return res.status(404).json({ error: 'Note not found' });
         }
